143ab4e2d06a817956daf83911cb788a7887a015
Lars Rosenquist <lrosenquist@pivotal.io>
2018-08-17 13:43:57 +0200

    Initial version.

---
+++ src/main/java/io/pivotal/cf/circuitbreakerdemo/CircuitBreakerDemoApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.cf.circuitbreakerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerDemoApplication.class, args);
+	}
+}
